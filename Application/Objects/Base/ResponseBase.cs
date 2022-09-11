b22df42c50150abea7513e7abf97661ee3b98119
guirms <guilhermesantana84@hotmail.com>
2022-09-11 11:12:11 -0300

    Criando camada application

---
+++ Application/Objects/Base/ResponseBase.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Application.Objects.Base;
+
+public class ResponseBase<T> where T: class 
+{
+    public bool Sucesso;
+    public string Mensagem;
+    public T? JsonRetorno;
+    
+    public  ResponseBase(bool sucesso, string mensagem, T? jsonRetorno = default)
+    {
+        Sucesso = sucesso;
+        Mensagem = mensagem;
+        JsonRetorno = jsonRetorno;
+    }
+    
+    public static JsonResult Responder(bool sucesso, string msg, object data)
+    {
+        return BigJson(new
+        {
+            sucesso = sucesso,
+            mensagem = msg,
+            data = data,
+        });
+    }
+    
+    public static JsonResult BigJson(object data)
+    {
+        return new JsonResult(data);
+    }
+}
