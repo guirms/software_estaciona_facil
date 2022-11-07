440a7652f97a778006064797cfdd522fd169f5be
guirms <guilhermesantana84@hotmail.com>
2022-11-06 21:41:50 -0300

    Criando rota de tela principal

--- Front-end/EstacionaFacil/src/app/app-routing.module.ts
+++ Front-end/EstacionaFacil/src/app/app-routing.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TelaCadastroComponent } from './Paginas/tela-cadastro/tela-cadastro.component';
 import { TelaLoginComponent } from './Paginas/tela-login/tela-login.component';
+import { TelaPrincipalComponent } from './Paginas/tela-principal/tela-principal.component';
 
-const routes: Routes = [{
-  path: '', component: TelaLoginComponent,
-},
-{
-  path: 'cadastro', component: TelaCadastroComponent
-}];
+const routes: Routes = [
+  {
+    // path: '', component: TelaLoginComponent,
+    path: '', component: TelaPrincipalComponent,
+  },
+  {
+    path: 'cadastro', component: TelaCadastroComponent
+  },
+  {
+    path: 'principal', component: TelaPrincipalComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Front-end/EstacionaFacil/src/app/app.module.ts
+++ Front-end/EstacionaFacil/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { TelaLoginComponent } from './Paginas/tela-login/tela-login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TelaCadastroComponent } from './Paginas/tela-cadastro/tela-cadastro.component';
 import { HttpClientModule } from '@angular/common/http';
+import { TelaPrincipalComponent } from './Paginas/tela-principal/tela-principal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TelaLoginComponent,
-    TelaCadastroComponent
+    TelaCadastroComponent,
+    TelaPrincipalComponent
   ],
   imports: [
     BrowserModule,
